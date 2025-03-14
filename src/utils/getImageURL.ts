bc7f4f59f45ceb04eaa94e4f42a551ca8fcae50b
alanb4rt <al4n.b4rt@gmail.com>
2025-03-14 16:37:10 +0100

    fix: include image assets in deployment build

--- src/utils/getImageURL.ts
+++ src/utils/getImageURL.ts
@@ -1,4 +1,3 @@
 export function getImageURL(path: string): string {
-  const cleanURL = path.substring(2);
-  return new URL(`../${cleanURL}`, import.meta.url).href;
+  return new URL(`../assets/images/${path}`, import.meta.url).href;
 }
